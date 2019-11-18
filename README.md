258010d36a4db222a416721d5a55f5553b6458dd
Per Mårtensson <per.martensson@yodiwo.com>
2019-11-18 19:18:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# For new versions, please go to [rak_common_for_gateway](https://github.com/RAKWireless/rak_common_for_gateway)
+
+# RAK2247&RAK833-LoRaGateway-RPi-USB
+
+based on the latest SX1301 driver [lora_gateway](https://github.com/Lora-net/lora_gateway) v5.0.1 and semtech [packet_forwarder](https://github.com/Lora-net/packet_forwarder) v4.0.1  
+
+Note: This project is only used for USB interface of RAK833 + Raspberry Pi, if you want to use SPI interface of RAK833 + Raspberry Pi, please use this project:
+https://github.com/RAKWireless/RAK2245-RAK831-LoRaGateway-RPi-Raspbian-OS. But you should notice the reset GPIO pin definition which is the value of SX1301_RESET_BCM_PIN macro in the start.sh file, it is pin17 now, and you can modify this value according to your hardware.
+
+##	Supported platforms
+
+This project currently provides support for the below platforms.
+
+* RAK833/RAK2247
+
+##	Changelog
+
+2019-06-18 RAK V2.8
+
+* 1.Add eth0 config
+* 2.After the network is disconnected, lora_pkt_fwd restarts
+* 3.Modify loraserver.toml for us915 and cn 470
+* 4.Streamlined local_conf.json files
+
+2019-05-06 RAK V2.1R
+
+* Add loraserver
+* Add AP mode
+
+##	Installation procedure
+
+step1 : Download and install [Raspbian Stretch LITE](https://www.raspberrypi.org/downloads/raspbian/) 
+
+
+step2 : Clone the installer and start the installation
+
+      $ git clone https://github.com/sm6uax/yodiwo-gatewayv3.git ~/yodiwo-loragatewayv3
+      $ cd ~/yodiwo-loragatewayv3
+      $ sudo ./install.sh
+
+step3 : Now you have a running gateway after restart! in additon you can check the log info at /var/log/syslog.
---
+++ lora/install.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# Stop on the first sign of trouble
+set -e
+
+if [ $UID != 0 ]; then
+    echo "ERROR: Operation not permitted. Forgot sudo?"
+    exit 1
+fi
+
+SCRIPT_DIR=$(pwd)
+
+VERSION="master"
+if [[ $1 != "" ]]; then VERSION=$1; fi
+
+echo "The Things Network Gateway installer"
+echo "Version $VERSION"
+
+# Request gateway configuration data
+# There are two ways to do it, manually specify everything
+# or rely on the gateway EUI and retrieve settings files from remote (recommended)
+echo "Gateway configuration:"
+
+if [ ! -d "/usr/local/rak/gateway-config-info" ]; then mkdir "/usr/local/rak/gateway-config-info" -p ; fi
+
+# Try to get gateway ID from MAC address
+# First try eth0, if that does not exist, try wlan0 (for RPi Zero)
+GATEWAY_EUI_NIC="eth0"
+if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
+    GATEWAY_EUI_NIC="wlan0"
+fi
+
+if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
+    echo "ERROR: No network interface found. Cannot set gateway ID."
+    exit 1
+fi
+
+GATEWAY_EUI=$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3"FFFE"$4$5$6}')
+GATEWAY_EUI=${GATEWAY_EUI^^} # toupper
+
+echo "Detected EUI $GATEWAY_EUI from $GATEWAY_EUI_NIC"
+
+# Check dependencies
+echo "Installing dependencies..."
+apt-get -y install git libftdi-dev libusb-dev
+
+# Build libraries
+
+if [ ! -d libmpsse ]; then
+    git clone https://github.com/devttys0/libmpsse.git
+fi
+pushd libmpsse/src
+./configure --disable-python
+make
+make install
+ldconfig
+popd
+
+# Install LoRaWAN packet forwarder repositories
+INSTALL_DIR="/opt/yodiwo-gatewayv3"
+if [ ! -d "$INSTALL_DIR" ]; then mkdir $INSTALL_DIR; fi
+pushd $INSTALL_DIR
+
+# Build LoRa gateway app
+
+if [ ! -d lora_gateway ]; then
+    git clone https://github.com/sm6uax/lora_gateway.git
+fi
+pushd lora_gateway
+
+cp ./libloragw/99-libftdi.rules /etc/udev/rules.d/99-libftdi.rules
+cp $SCRIPT_DIR/loragw_spi.ftdi.c ./libloragw/src/
+cp $SCRIPT_DIR/Makefile-gw-lib ./libloragw/Makefile
+cp $SCRIPT_DIR/Makefile-lbt-test ./util_lbt_test/Makefile
+cp $SCRIPT_DIR/Makefile-pkt-logger ./util_pkt_logger/Makefile
+cp $SCRIPT_DIR/Makefile-spectral-scan ./util_spectral_scan/Makefile
+cp $SCRIPT_DIR/Makefile-spi-stress ./util_spi_stress/Makefile
+cp $SCRIPT_DIR/Makefile-tx-continuous ./util_tx_continuous/Makefile
+cp $SCRIPT_DIR/Makefile-tx-test ./util_tx_test/Makefile
+cp $SCRIPT_DIR/library.cfg ./libloragw/
+cp $SCRIPT_DIR/Makefile-gw ./Makefile
+
+sed -i -e 's/CFG_SPI= native/CFG_SPI= ftdi/g' ./libloragw/library.cfg
+
+make
+popd
+
+# Build packet forwarder
+if [ ! -d packet_forwarder ]; then
+    git clone https://github.com/sm6uax/packet_forwarder.git
+fi
+pushd packet_forwarder
+
+cp $SCRIPT_DIR/Makefile-pk ./lora_pkt_fwd/Makefile
+cp $SCRIPT_DIR/update_gwid.sh ./lora_pkt_fwd/update_gwid.sh
+
+make
+
+popd
+
+
+LOCAL_CONFIG_FILE=$INSTALL_DIR/packet_forwarder/lora_pkt_fwd/local_conf.json
+
+#config local_conf.json
+
+    echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"$GATEWAY_EUI\" \n\t}\n}" >$LOCAL_CONFIG_FILE
+
+echo "Gateway EUI is: $GATEWAY_EUI"
+echo "The hostname is: $NEW_HOSTNAME"
+echo "Open Yodiwo console and register your gateway using your EUI: https://cyan.yodiwo/gateways"
+echo
+echo "Installation completed."
+
+#
+cd $SCRIPT_DIR
+cp lora_conf /etc/ -rf
+cp lora_conf/serial/global_conf.eu_863_870.json $INSTALL_DIR/packet_forwarder/lora_pkt_fwd/global_conf.json
+sed -i "s/^.*server_address.*$/\t\"server_address\": \"127.0.0.1\",/" /opt/yodiwo-gatewayv3/packet_forwarder/lora_pkt_fwd/global_conf.json
+cp gateway-version* /usr/bin/
+cp gateway-config /usr/bin/
+
+# Start packet forwarder as a service
+cp ./start.sh $INSTALL_DIR/packet_forwarder/lora_pkt_fwd/
+cp ./set_eui.sh $INSTALL_DIR/packet_forwarder/lora_pkt_fwd/
+cp $SCRIPT_DIR/yodiwo-gateway.service /lib/systemd/system/
+systemctl enable yodiwo-gateway.service
+
+
+
